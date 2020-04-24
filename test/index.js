72a70fc9d46c4d1e84fede8b3455e9c3011224e3
Andy Edwards <jedwards@fastmail.com>
2020-04-24 15:37:16 -0500

    test: only invalidate mocha folder

--- test/index.js
+++ test/index.js
@@ -4,19 +4,17 @@ const invalidateRequireCache = require('..')
 const touch = require('touch')
 const path = require('path')
 
-const nodeModules = path.resolve(__dirname, '..', 'node_modules')
-
 describe('invalidateRequireCache', function() {
   it('works', function() {
     expect(require.cache[require.resolve('mocha')]).to.exist
-    invalidateRequireCache(nodeModules)
+    invalidateRequireCache(path.dirname(require.resolve('mocha')))
     expect(require.cache[require.resolve('mocha')]).not.to.exist
     require('mocha')
     expect(require.cache[require.resolve('mocha')]).to.exist
-    invalidateRequireCache(nodeModules)
+    invalidateRequireCache(path.dirname(require.resolve('mocha')))
     expect(require.cache[require.resolve('mocha')]).to.exist
-    touch.sync(nodeModules)
-    invalidateRequireCache(nodeModules)
+    touch.sync(path.dirname(require.resolve('mocha')))
+    invalidateRequireCache(path.dirname(require.resolve('mocha')))
     expect(require.cache[require.resolve('mocha')]).not.to.exist
   })
 })
